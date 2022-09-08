e0baf4d65c4845d0c8b5148cab578c105ae08c4f
marcondesu <rkatchau@gmail.com>
2022-09-08 10:36:39 -0300

    fix: organizando funcoes com arquivo header

---
+++ Pilhas/pilha1.c
@@ -0,0 +1,53 @@
+#include "pilha1.h"
+
+int getTopo(Pilha *p) {
+	return p->topo;
+}
+
+bool pilhaCheia(Pilha *p) {
+	return getTopo(p) == p->tamanho - 1;
+}
+
+bool pilhaVazia(Pilha *p) {
+	return p->topo == -1;
+}
+
+Pilha* criar_pilha(int tamanho_da_pilha) {
+	Pilha *p = (Pilha*)malloc(sizeof(Pilha));
+	
+	p->tamanho = tamanho_da_pilha;
+	p->topo = -1;
+	p->vetor = (char*)malloc(tamanho_da_pilha * sizeof(char));
+
+    return p;
+}
+
+void push(Pilha *p, char valor) {
+	if (pilhaCheia(p)) {
+        printf("Erro! A pilha esta cheia\n");
+        abort();
+	}
+
+	p->vetor[getTopo(p) + 1] = valor;
+	p->topo++;
+}
+
+char pop(Pilha *p) {
+	if (pilhaVazia(p)) {
+	    printf("Erro! A pilha esta vazia\n");
+        abort();
+	}
+
+    char valor_topo = p->vetor[getTopo(p)];
+
+    p->vetor[getTopo(p)] = 0;
+	p->topo--;
+
+    return valor_topo;
+}
+
+void destruir(Pilha *p) {
+	free((p)->vetor);
+	free(p);
+	p = NULL;
+}
---
+++ Pilhas/pilha1.h
@@ -0,0 +1,28 @@
+#ifndef PILHA1_H
+#define PILHA1_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct pilha {
+	int tamanho;	// tamanho da pilha
+	int topo;		// index do elemento mais acima na pilha
+	char *vetor;
+} Pilha;
+
+int getTopo(Pilha *p);
+
+bool pilhaCheia(Pilha *p);
+
+bool pilhaVazia(Pilha *p);
+
+Pilha* criar_pilha(int tamanho_da_pilha);
+
+void push(Pilha *p, char valor);
+
+char pop(Pilha *p);
+
+void destruir(Pilha *p);
+
+#endif
