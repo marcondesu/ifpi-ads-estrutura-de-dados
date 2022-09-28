7cb628c33b4af98981664d0a646299a29be5db42
marcondesu <rkatchau@gmail.com>
2022-09-28 11:32:40 -0300

    add: implementação de pilha com nós por meio de classe

--- Pilhas/pilha-incompleta.cpp
+++ Pilhas/pilha-incompleta.cpp
@@ -1,42 +1,69 @@
 #include <stdio.h>
 #include <iostream>
 #include <stdlib.h>
-#include <string.h>
+#include <string>
 
 using namespace std;
 
-class No{
+class No {
 	public:
 		char nome;
 		No *prox;
-		
-		No(char n){
+
+		No(char n) {
 			nome = n;
 			prox = NULL;
 		}
 };
 
-class Pilha{
+class Pilha {
 	public:
 		No *topo;
 		
-		Pilha(){
+		Pilha() {
 			topo = NULL;
 		}
 		
-		void push(char n){
-		
+		void push(char n) {
+			No *novo = new No(n);
+
+			if (isEmpty()) {
+				topo = novo;
+			} else {
+				novo->prox = topo;
+				topo = novo;
+			}
 		}
 		
-		char pop(){
+		char pop() {
+			No *remove = topo;
+			char item = topo->nome;
 			
+			topo = topo->prox;
+
+			free(remove);
+
+			return item;
 		}
 		
-		int isEmpty(){
-			
+		int isEmpty() {
+			return topo == NULL;
 		}
 };
 
-int main(){
-	
+int main() {
+	Pilha* pilha = new Pilha();
+
+	printf("isEmpty() = %d\n\n", pilha->isEmpty());
+
+	pilha->push('a');
+	pilha->push('b');
+	pilha->push('c');
+
+	printf("Pop: %c\n", pilha->pop());
+	printf("Pop: %c\n", pilha->pop());
+
+	printf("\nisEmpty() = %d\n", pilha->isEmpty());
+
+	printf("\nTopo: %c", pilha->topo->nome);
 }
