3c1205a2872d750b32eff283aeb32e76ba3744fa
marcondesu <rkatchau@gmail.com>
2022-10-07 21:56:49 -0300

    add: pasta de filas

---
+++ Filas/fila-incompleta.cpp
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <iostream>
+#include <stdlib.h>
+#include <string.h>
+
+using namespace std;
+
+class No {
+	public:
+		char nome;
+		No *prox;
+
+		No(char n) {
+			nome = n;
+			prox = NULL;
+		}
+};
+
+class Fila {
+	public:
+		No *inicio;
+		No *fim;
+		int max;
+		int total;
+
+		Fila(int m) {
+			inicio = NULL;
+			fim = NULL;
+			max = m;
+			total = 0;
+		}
+
+		void append(char n) {
+			if (isEmpty()) {
+				No *novo = new No(n);
+
+				inicio = novo;
+				total++;
+			} else if (!isFull()) {
+				No *novo = new No(n);
+
+				inicio->prox = novo;
+				total++;
+			}
+		}
+
+		// char pop() {}
+
+		bool isEmpty() {
+			return inicio == NULL;
+		}
+
+		bool isFull() {
+			return fim != NULL;
+		}
+
+	    void popAll() {}
+};
+
+int main() {
+	Fila fila;
+
+	fila.append('a');
+	fila.append('b');
+	fila.append('c');
+	fila.append('d');
+	fila.append('e');
+	fila.append('f');
+	fila.append('g');
+
+	printf("%c", fila.inicio->prox->nome);
+}
